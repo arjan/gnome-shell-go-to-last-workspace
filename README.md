bf2d89ad42ed4bda68181dfddfaa74c0079b62d6
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-06-05 20:48:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Go To Last Workspace
+====================
+
+Extension for the Gnome Shell to quickly switch back to the previous
+workspace you were using.
+
+It installs a keybinding which defaults to <Super>escape. Repeatedly
+pressing <Super>escape causes the active workspace to toggle between
+the last two selected workspaces.
+
+Only tested with Gnome version 3.20.
---
+++ extension.js
@@ -0,0 +1,51 @@
+const Meta = imports.gi.Meta;
+const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
+
+const SHORTCUT_KEY = 'shortcut-key';
+
+const Self = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Self.imports.convenience;
+
+let settings = Convenience.getSettings();
+
+var currentWorkspace = -1;
+var lastWorkspace = -1;
+
+function goToLastWorkspace() {
+  if (lastWorkspace < 0) {
+    return;
+  }
+
+  let ws = global.screen.get_workspace_by_index(lastWorkspace);
+  ws.activate(0);
+}
+
+
+function init() {
+}
+
+let signals = [];
+
+function enable() {
+  var ModeType = Shell.hasOwnProperty('ActionMode') ? Shell.ActionMode : Shell.KeyBindingMode;
+  Main.wm.addKeybinding(SHORTCUT_KEY, settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, goToLastWorkspace);
+
+  signals.push(global.screen.connect('workspace-switched', function(screen, prev, current, direction) {
+    lastWorkspace = currentWorkspace;
+    currentWorkspace = current;
+  }));
+}
+
+function disable() {
+  // clean up
+  Main.wm.removeKeybinding(SHORTCUT_KEY);
+  
+  let i = signals.length;
+  while (i--) {
+    global.screen.disconnect(signals.pop());
+  }
+  
+}
+
+
